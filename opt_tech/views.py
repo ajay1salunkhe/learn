0db3382de8056c10573cf3002befbea79a27e2e6
ajay1salunkhe <mail2ajaysalunkhe123@gmail.com>
2019-01-20 06:30:18 -0800

    error test 8

--- opt_tech/views.py
+++ opt_tech/views.py
@@ -45,6 +45,7 @@ def ass_problem(request):
                 
                 print("l2 = ",l2)
                 print("l1 = ",l1)
+                '''
                 import sys
                 sys.path.append("numpy_path")
                 messages.success(request,"success")
@@ -52,7 +53,7 @@ def ass_problem(request):
                 
                 minc=np.min(l2, axis=0)
                 l3 = list(l2-minc)
-                minc  = list(minc)
+                minc  = list(minc)'''
                 return render(request, 'opt_tech/ass_problem_ans.html', {"l1":list(l1),"l2":list(l2),"l3":list(l3),"minr":list(minr),"minc":list(minc),"rows":range(1,rows+1),"cols":range(1,cols+1)})  
         except Exception as e:
             print(str(e))
